f6ae1deec74ae4d5fd08f66bbca7b4cdcbda2162
SREEJITH-P-B <33229709+SREEJITH-P-B@users.noreply.github.com>
2020-03-18 15:15:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import urllib3.request
 from flask import Flask, flash, request, redirect, render_template
 UPLOAD_FOLDER = './'
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
